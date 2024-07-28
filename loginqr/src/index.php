9ba7175b45557c0931cb9616a270216cff04bbc2
Natthee Prommool <38148690+nattheepr@users.noreply.github.com>
2024-07-28 20:47:29 +0700

    up

---
+++ loginqr/src/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<?php
+    include("layout/navbar.php");
+?>  
+<div class="card" style="width: 36rem;">
+  <img src="https://scontent.fbkk29-8.fna.fbcdn.net/v/t39.30808-6/449827968_26102427402736722_2532618010977970141_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=LI3NTcKQ8noQ7kNvgG8xjZb&_nc_ht=scontent.fbkk29-8.fna&oh=00_AYDLgKeyx5VAHNl1iPFOfG3JUzN9_hEhgV_MMv_Y9KmgEg&oe=66AC055A" class="card-img-top" alt="baszy">
+  <div class="card-body">
+    <h5 class="card-title">id: 65130695</h5>
+    <p class="card-text">name: kittinun roonsungneon</p>
+    <a href="#" class="btn btn-primary">Hello World!</a>
+  </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
